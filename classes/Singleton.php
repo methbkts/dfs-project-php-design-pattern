9c562296124758d0e842d551efa8ff8f40aefc2b
methbkts <30674934+methbkts@users.noreply.github.com>
2019-10-14 14:35:18 +0200

    add Singleton sample Class

---
+++ classes/Singleton.php
@@ -0,0 +1,36 @@
+<?php
+ 
+class Singleton {
+ 
+  /**
+   * @var Singleton
+   * @access private
+   * @static
+   */
+   private static $_instance = null;
+ 
+   /**
+    * Constructeur de la classe
+    *
+    * @param void
+    * @return void
+    */
+   private function __construct() {  
+   }
+ 
+   /**
+    * Méthode qui crée l'unique instance de la classe
+    * si elle n'existe pas encore puis la retourne.
+    *
+    * @param void
+    * @return Singleton
+    */
+   public static function getInstance() {
+ 
+     if(is_null(self::$_instance)) {
+       self::$_instance = new Singleton();  
+     }
+ 
+     return self::$_instance;
+   }
+}
