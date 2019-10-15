b19f9c5f85e5a6a6e490c242525367c1192bdf6c
methbkts <30674934+methbkts@users.noreply.github.com>
2019-10-15 12:43:02 +0200

    hotfix header

--- public/layouts/header.php
+++ public/layouts/header.php
@@ -62,7 +62,7 @@
         <div class="container d-flex h-100 align-items-center">
             <div class="mx-auto text-center">
                 <?php
-                if ($currentPageTitle == "Homepage") { ?>
+                if ($currentPageTitle == "Bienvenue") { ?>
                     <h1 class="mx-auto my-0 text-uppercase mb-4">Bienvenue</h1>
                     <!-- <h2 class="text-white-50 mx-auto mt-2 mb-5">A free, responsive, one page Bootstrap theme created by Start Bootstrap.</h2> -->
                     <a href="#farmers" class="btn btn-primary js-scroll-trigger">Trouvez votre fermier</a>
