2b37460de6dbf4ada7ab71b6cdee812992a6a0a3
methbkts <30674934+methbkts@users.noreply.github.com>
2019-10-16 18:41:33 +0200

    editFarmerAction not finished

--- controllers/editFarmerAction.php
+++ controllers/editFarmerAction.php
@@ -1,15 +1,53 @@
 <?php
 
-// editFarmerAction
-// $name = $_POST['name'];
-// $weapon = $_POST['weapon'];
-// $special = $_POST['special'];
+$currentFarmer = Farmer::getOneFarmer($farmer_id);
+
+if (empty($name)) {
+    $name = $currentFarmer->name;
+} else {
+    $name = Functions::test_input($_POST['name']);
+}
+
+if (empty($description)) {
+    $description = $currentFarmer->description;
+} else {
+    $description = Functions::test_input($_POST['description']);
+}
+
+// $location = Functions::test_input($_POST['location']);
 // if (empty($_POST['image'])) {
-//     $image = 'https://picsum.photos/300/200';
+//     $image = '/public/img/default-profile.png';
 // } else {
-//     $image = $_POST['image'];
+//     $image = Functions::test_input($_POST['image']);
 // }
-// $role_id = intval($_POST['role']);
-// $race_id = intval($_POST['race']);
+
+if (!empty($price)) {
+    $price = $currentProduct->price;
+} else {
+    intval($_POST['price']);
+}
+
+if (!empty($quantity)) {
+    $quantity = $currentProduct->quantity;
+} else {
+    intval($_POST['quantity']);
+}
+
+if (!empty($type_id)) {
+    $type_id = $currentProduct->type_id;
+} else {
+    intval($_POST['type_id']);
+}
+
+if (!empty($farmer_id)) {
+    $farmer_id = $currentProduct->farmer_id;
+} else {
+    intval($_POST['farmer_id']);
+}
+// $farmer_id = intval($_POST['farmer_id']);
+
+// Functions::dd($farmer_id);
+
+Product::updateProduct($request_uri[1], $name, $price, $image, $quantity, $type_id, $farmer_id);
 
 Farmer::updateFarmer($request_uri[1]);
