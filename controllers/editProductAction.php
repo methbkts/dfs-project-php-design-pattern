65a9e23d68e951c5813bc3433f8ccb720d04f43f
lmanso <30408665+lmanso@users.noreply.github.com>
2019-10-16 18:05:31 +0200

    fix

--- controllers/editProductAction.php
+++ controllers/editProductAction.php
@@ -1,11 +1,45 @@
 <?php
 
-$name = Functions::test_input($_POST['name']);
-$image = Functions::test_input($_POST['image']);
-$price = intval($_POST['price']);
-$quantity = $_POST['quantity'];
-$type_id = intval($_POST['type_id']);
-$farmer_id = intval($_POST['farmer_id']);
+$currentProduct = Product::getOneProduct($product_id);
+// Functions::dd($currentProduct);
+
+
+if(!empty($name)){
+    $name = $currentProduct->name;
+} else {
+    $name = Functions::test_input($_POST['name']);
+}
+
+if(!empty($image)){
+    $image = $currentProduct->image;
+} else {
+    $image = Functions::test_input($_POST['image']);
+}
+
+if(!empty($price)){
+    $price = $currentProduct->price;
+} else {
+    intval($_POST['price']);
+}
+
+if(!empty($quantity)){
+    $quantity = $currentProduct->quantity;
+} else {
+    intval($_POST['quantity']);
+}
+
+if(!empty($type_id)){
+    $type_id = $currentProduct->type_id;
+} else {
+    intval($_POST['type_id']);
+}
+
+if(!empty($farmer_id)){
+    $farmer_id = $currentProduct->farmer_id;
+} else {
+    intval($_POST['farmer_id']);
+}
+// $farmer_id = intval($_POST['farmer_id']);
 
 // Functions::dd($farmer_id);
 
--- public/views/editProduct.php
+++ public/views/editProduct.php
@@ -1,7 +1,7 @@
 <?php
 $type = Product::getAllTypes();
 $farmer = Farmer::getAllFarmers();
-$productUpadte = Product::getOneProduct($product_id);
+$productUpdate = Product::getOneProduct($product_id);
 // Functions::dd($farmer);
 ?>
 <section id="section" class="projects-section bg-light">
@@ -9,20 +9,20 @@ $productUpadte = Product::getOneProduct($product_id);
         <div class="row align-items-center no-gutters mb-4 mb-lg-5">
             <div class="col-xl-4 col-lg-2">
                 <div class="featured-text text-center text-lg-left">
-                    <h4>Modifier les <?= $productUpadte->name ?></h4>
-                    <p class="text-black-50 mb-0">Vous pouvez modifier les <?= $productUpadte->name ?> avec ce formulaire</p>
+                    <h4>Modifier les <?= $productUpdate->name ?></h4>
+                    <p class="text-black-50 mb-0">Vous pouvez modifier les <?= $productUpdate->name ?> avec ce formulaire</p>
                 </div>
             </div>
             <div class="row align-items-center no-gutters mb-4 mb-lg-5 displayProducts updateForm">
                 <div class="col-xl-6 col-lg-3">
-                    <img class="img-fluid" src="<?= $productUpadte->image ?>" alt="">
+                    <img class="img-fluid" src="<?= $productUpdate->image ?>" alt="">
                 </div>
                 <div class="col-lg-6">
                     <div class="bg-black text-center h-100 project">
                         <div class="d-flex h-100">
                             <div class="project-text w-100 my-auto text-center text-lg-left">
-                                <h4 class="text-white"><?= $productUpadte->name ?> de <?= $productUpadte->farmer ?></span></h4>
-                                <p class="mb-0 text-muted"> <span class="text-white-50">Les <?= $productUpadte->name ?> de <?= $productUpadte->farmer ?> sont propose a : <?= $productUpadte->price ?> €</span>, il en reste : <span class="text-white-50"><?= $productUpadte->quantity ?></p>
+                                <h4 class="text-white"><?= $productUpdate->name ?> de <?= $productUpdate->farmer ?></span></h4>
+                                <p class="mb-0 text-muted"> <span class="text-white-50">Les <?= $productUpdate->name ?> de <?= $productUpdate->farmer ?> sont propose a : <?= $productUpdate->price ?> €</span>, il en reste : <span class="text-white-50"><?= $productUpdate->quantity ?></p>
                                 <hr class="d-none d-lg-block mb-0 ml-0">
                             </div>
                         </div>
