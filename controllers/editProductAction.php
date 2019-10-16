df2593809bf02a47328ccc6631f91ed512c0ed7d
methbkts <30674934+methbkts@users.noreply.github.com>
2019-10-16 09:46:46 +0200

    add Product action controllers

---
+++ controllers/editProductAction.php
@@ -0,0 +1,15 @@
+<?php
+
+// editFarmerAction
+// $name = $_POST['name'];
+// $weapon = $_POST['weapon'];
+// $special = $_POST['special'];
+// if (empty($_POST['image'])) {
+//     $image = 'https://picsum.photos/300/200';
+// } else {
+//     $image = $_POST['image'];
+// }
+// $role_id = intval($_POST['role']);
+// $race_id = intval($_POST['race']);
+
+Product::updateProduct($request_uri[1]);
---
+++ controllers/removeProductAction.php
@@ -0,0 +1,15 @@
+<?php
+
+// removeFarmerAction
+// $name = $_POST['name'];
+// $weapon = $_POST['weapon'];
+// $special = $_POST['special'];
+// if (empty($_POST['image'])) {
+//     $image = 'https://picsum.photos/300/200';
+// } else {
+//     $image = $_POST['image'];
+// }
+// $role_id = intval($_POST['role']);
+// $race_id = intval($_POST['race']);
+
+Farmer::deleteFarmer($request_uri[1]);
