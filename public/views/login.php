7562e93c23b60c1ebaa1b1438c36d39286f98076
lmanso <30408665+lmanso@users.noreply.github.com>
2019-10-17 15:32:20 +0200

    img login

---
+++ public/views/login.php
@@ -0,0 +1,18 @@
+<section id="section" class="projects-section bg-light">
+    <div class="container">
+        <form class="form-signin">
+            <img class="" src="/public/img/logo_transparent.png" alt="" width="30%" height="30%">
+            <h1 class="h3 mb-3 font-weight-normal">Connectez-vous</h1>
+            <label for="inputEmail" class="sr-only">Email ou pseudo</label>
+            <input type="email" id="inputEmail" class="form-control" placeholder="Email ou pseudo" required autofocus>
+            <label for="inputPassword" class="sr-only">Password</label>
+            <input type="password" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
+            <div class="checkbox mb-3">
+                <label>
+                    <input type="checkbox" value="remember-me"> Se souvenir de moi
+                </label>
+            </div>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Connexion</button>
+        </form>
+    </div>
+</section>
