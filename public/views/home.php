d2fa3e86fcea18d7cd346cef7cbe9ea0c3e9c25b
lmanso <30408665+lmanso@users.noreply.github.com>
2019-10-17 13:33:23 +0200

    pricing

--- public/views/home.php
+++ public/views/home.php
@@ -8,18 +8,21 @@ $farmer = Farmer::getAllFarmers();
             <div class="col-lg-4">
                 <div class="card mb-5 mb-lg-0">
                     <div class="card-body">
-                        <h5 class="card-title text-muted text-uppercase text-center">Free</h5>
-                        <h6 class="card-price text-center">$0<span class="period">/mois</span></h6>
+                        <h5 class="card-title text-muted text-uppercase text-center">Duo</h5>
+                        <h6 class="card-price text-center">15€<span class="period">/mois</span></h6>
                         <hr>
                         <ul class="fa-ul">
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Single User</li>
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>5GB Storage</li>
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
-                            <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Unlimited Private Projects</li>
-                            <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Dedicated Phone Support</li>
-                            <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Free Subdomain</li>
-                            <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status Reports</li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>Un panier pour deux personnes
+                                    &nbsp;
+
+                                </strong></li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span>fruits</li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span>légumes</li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Panier en point de collecte</li>
+                            <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Herbes aromatiques</li>
+                            <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Fiche personnalisé pour savoir quels produits vous ne souhaitez pas recevoirt</li>
+                            <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Choix de la date de retrait du panier</li>
+                            <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Livraisons a domicile ou au travail</li>
                         </ul>
                         <a href="#" class="btn btn-block btn-primary text-uppercase">S'abonner</a>
                     </div>
@@ -29,18 +32,18 @@ $farmer = Farmer::getAllFarmers();
             <div class="col-lg-4">
                 <div class="card mb-5 mb-lg-0">
                     <div class="card-body">
-                        <h5 class="card-title text-muted text-uppercase text-center">Plus</h5>
-                        <h6 class="card-price text-center">$9<span class="period">/mois</span></h6>
+                        <h5 class="card-title text-muted text-uppercase text-center">Famille</h5>
+                        <h6 class="card-price text-center">25€<span class="period">/mois</span></h6>
                         <hr>
                         <ul class="fa-ul">
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>5 Users</strong></li>
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>50GB Storage</li>
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Private Projects</li>
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Dedicated Phone Support</li>
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Free Subdomain</li>
-                            <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status Reports</li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>Un panier pour une famille de quatre personnes</strong></li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span>fruits</li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span>légumes</li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Panier en point de collecte</li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Herbes aromatiques</li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Fiche personnalisé pour savoir quels produits vous ne souhaitez pas recevoir</li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Choix de la date de retrait du panier</li>
+                            <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Livraisons a domicile ou au travail</li>
                         </ul>
                         <a href="#" class="btn btn-block btn-primary text-uppercase">S'abonner</a>
                     </div>
@@ -50,18 +53,18 @@ $farmer = Farmer::getAllFarmers();
             <div class="col-lg-4">
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title text-muted text-uppercase text-center">Pro</h5>
-                        <h6 class="card-price text-center">$49<span class="period">/mois</span></h6>
+                        <h5 class="card-title text-muted text-uppercase text-center">Premium</h5>
+                        <h6 class="card-price text-center">35€<span class="period">/mois</span></h6>
                         <hr>
                         <ul class="fa-ul">
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>Unlimited Users</strong></li>
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>150GB Storage</li>
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Private Projects</li>
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Dedicated Phone Support</li>
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>Unlimited</strong> Free Subdomains</li>
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Monthly Status Reports</li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>Un panier</strong> (profil au choix entre deux et quatre personnes)</li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span>fruits</li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span>légumes</li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Panier en point de collecte</li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Herbes aromatiques</li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Fiche personnalisé pour savoir quels produits vous ne souhaitez pas recevoir</li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Choix de la date de retrait du panier</li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Livraisons a domicile ou au travail</li>
                         </ul>
                         <a href="#" class="btn btn-block btn-primary text-uppercase">S'abonner</a>
                     </div>
