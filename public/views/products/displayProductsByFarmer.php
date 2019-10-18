b2e864121d66cdeba3fd255058183de20b24a2be
methbkts <30674934+methbkts@users.noreply.github.com>
2019-10-18 12:29:44 +0200

    fix: display products by farmer page

--- public/views/products/displayProductsByFarmer.php
+++ public/views/products/displayProductsByFarmer.php
@@ -1,6 +1,7 @@
 <?php
 
 ?>
+
 <section id="section" class="projects-section bg-light">
     <div class="container">
         <div class="row align-items-center no-gutters mb-4 mb-lg-5">
@@ -27,13 +28,8 @@
                                 <h4 class="text-white"><?= $value->name ?> de <?= $value->farmer ?></span></h4>
                                 <p class="mb-0 text-white">Les <?= $value->name ?> de <?= $value->farmer ?> sont proposé(e)s à <?= $value->price ?>€.</p>
                                 <p class="mb-0 text-white">Il en reste actuellement <?= $value->quantity ?>.</p>
-                                <!-- <p class="text-white-50"> pour voir plus de produits propose par <?= $value->farmer ?></p> -->
+
                                 <hr class="d-none d-lg-block mb-0 ml-0">
-                                <!-- <p>
-                                    <button type="button" class="btn btn-info"><i class="fas fa-binoculars"></i></button>
-                                    <a href="/editProduct?<?= $value->id ?>"><button type="button" value="<?= $value->id ?>" class="btn btn-warning"><i class="far fa-edit"></i></button></a>
-                                    <a href="/deleteProduct?<?= $value->id ?>"><button type="button" value="<?= $value->id ?>" class="btn btn-danger"><i class="far fa-trash-alt"></i></button></a>
-                                </p> -->
                             </div>
                         </div>
                     </div>
